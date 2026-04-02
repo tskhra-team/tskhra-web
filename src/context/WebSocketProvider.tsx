97b6ae0af3916a5aaa7057c93c2aae21e6b81ffc
mikheilikvaratskhelia <mikheili.kvaratskhelia@makingscience.com>
2026-04-02 17:04:32 +0400

    chore: url for prod

--- src/context/WebSocketProvider.tsx
+++ src/context/WebSocketProvider.tsx
@@ -48,7 +48,7 @@ export function WebSocketProvider({ children }: { children: ReactNode }) {
     }
 
     const newClient = new Client({
-      brokerURL: `ws://10.227.164.247:8081/ws?token=${token}`,
+      brokerURL: `wss://sessile-nettlelike-antoinette.ngrok-free.dev/ws?token=${token}`,
       onConnect: () => {
         newClient.subscribe("/user/queue/messages", async (message) => {
           const data = JSON.parse(message.body);
