172b838229b1d072acd7ee080f7ef664e543f48c
mikheilikvaratskhelia <mikheili.kvaratskhelia@makingscience.com>
2026-04-22 15:07:02 +0400

    feat: unhide and hide implementation for my items

--- src/Swapping/MyItems/MyItems.tsx
+++ src/Swapping/MyItems/MyItems.tsx
@@ -98,7 +98,7 @@ export default function MyItems() {
 
   return (
     <div className="bg-swap-bg">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <Button
           variant="link"
           className="mb-10"
